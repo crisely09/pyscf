574326bfe9c336b9c9da320562a582b8f98e0537
Cristina E. González-Espinoza <crisbeth46@gmail.com>
2019-11-29 10:32:49 +0100

    Added first implementation of Frozen-Density Embedding TDDFT. Tests missing.

--- pyscf/tddft/__init__.py
+++ pyscf/tddft/__init__.py
@@ -13,4 +13,66 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pyscf.tdscf import *
+from pyscf.tdscf import rhf
+from pyscf.tdscf.rks import TDRKS
+from pyscf.tddft import rks
+from pyscf import scf
+
+
+def FDETDA(mf):
+    if isinstance(mf, scf.uhf.UHF):
+        raise NotImplementedError('Only Restrited case available')
+       #mf = scf.addons.convert_to_uhf(mf)
+       #if getattr(mf, 'xc', None):
+       #    return uks.TDA(mf)
+       #else:
+       #    return uhf.TDA(mf)
+    else:
+        mf = scf.addons.convert_to_rhf(mf)
+        if getattr(mf, 'xc', None):
+            return rks.FDETDA(mf)
+        else:
+            raise NotImplementedError('Only DFT case.')
+           #return rhf.TDA(mf)
+
+def FDETDDFT(mf):
+    if isinstance(mf, scf.uhf.UHF):
+        raise NotImplementedError('Only Restrited case available')
+       #mf = scf.addons.convert_to_uhf(mf)
+       #if getattr(mf, 'xc', None):
+       #    if mf._numint.libxc.is_hybrid_xc(mf.xc):
+       #        return uks.TDDFT(mf)
+       #    else:
+       #        return uks.TDDFTNoHybrid(mf)
+       #else:
+       #    return uhf.TDHF(mf)
+    else:
+        mf = scf.addons.convert_to_rhf(mf)
+        if getattr(mf, 'xc', None):
+            if mf._numint.libxc.is_hybrid_xc(mf.xc):
+                return rks.FDETDDFT(mf)
+            else:
+                return rks.FDETDDFTNoHybrid(mf)
+        else:
+            raise NotImplementedError('Only DFT case.')
+           #return rhf.TDHF(mf)
+
+FDETD = FDETDDFT
+
+
+def FDERPA(mf):
+    return FDETDDFT(mf)
+
+def FDEdRPA(mf):
+    if isinstance(mf, scf.uhf.UHF):
+        raise NotImplementedError('Only Restrited case available')
+       #return uks.dRPA(mf)
+    else:
+        return rks.FDEdRPA(mf)
+
+def FDEdTDA(mf):
+    if isinstance(mf, scf.uhf.UHF):
+        raise NotImplementedError('Only Restrited case available')
+       #return uks.dTDA(mf)
+    else:
+        return rks.FDEdTDA(mf)
